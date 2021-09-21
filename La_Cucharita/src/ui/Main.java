8a74c36ccd395ca2e699e8228d0ee6d2674684e8
Santiago Arboleda Velasco <santiagoarboledavelasco@gmail.com>
2021-09-21 00:08:28 -0500

    Test of COOP

--- La_Cucharita/src/ui/Main.java
+++ La_Cucharita/src/ui/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-		//Testeo codificacion COOP
+		//Testeo codificacion COOPfd
 		
 	}
 
