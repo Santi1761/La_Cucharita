9ba1ac64394eece5df3b26d80921c8858dd29105
juanj <juanjoserojasmayor12@gmail.com>
2021-09-21 21:33:51 -0500

    Partner proof 2

--- La_Cucharita/src/ui/Main.java
+++ La_Cucharita/src/ui/Main.java
@@ -8,12 +8,12 @@ public class Main extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-<<<<<<< HEAD
 		// Hola 
-=======
-		//Testeo codificacion COOPfd
-		
->>>>>>> branch 'master' of https://github.com/Santi1761/La_Cucharita
+		// buenas noches
+		//La enseñanza eficaz del Evangelio nutrirá
+		//y edificará a los demás. Edificará su fe y les dará la confianza que necesitan 
+		//para enfrentar los retos de la vida. Les alentará a abandonar el pecado y a obedecer
+		//los mandamientos.
 	}
 
 }
